63f8bdcb49bb47bba75842c3d9f2d2bc427f8c06
Jason <leon___bek@mail.ru>
2020-04-29 18:06:52 +0500

    added todo

--- src/main/kotlin/uz/kvikk/linker/LinkerApplication.kt
+++ src/main/kotlin/uz/kvikk/linker/LinkerApplication.kt
@@ -9,3 +9,24 @@ class LinkerApplication
 fun main(args: Array<String>) {
 	runApplication<LinkerApplication>(*args)
 }
+
+/** todo index.html yasash kerak
+ * <!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+
+<script>
+// Empty
+</script>
+
+<script type="text/javascript">
+window.location.href = "https://play.google.com/store/apps/details?id=com.dimasta.feelqueen"
+</script>
+<title>Redirecting...</title>
+</head>
+<body>
+<p>If you are not redirected automatically, follow this <a href="https://play.google.com/store/apps/details?id=com.dimasta.feelqueen">link to redirect</a>.</p>
+</body>
+</html>
+ */
