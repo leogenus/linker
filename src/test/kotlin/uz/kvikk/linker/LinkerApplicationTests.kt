51a7a7e08b01f5cd2fd2539e6e09e5386d3f9027
Jason <leon___bek@mail.ru>
2020-04-29 18:04:31 +0500

    Start

---
+++ src/test/kotlin/uz/kvikk/linker/LinkerApplicationTests.kt
@@ -0,0 +1,13 @@
+package uz.kvikk.linker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LinkerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
